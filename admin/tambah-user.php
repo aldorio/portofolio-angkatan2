5201c3787ec3ebc3a069b40bf2f2b27eb88fc1ce
afnyyy <afny27@gmail.com>
2025-05-19 11:02:50 +0700

    First commit

---
+++ admin/tambah-user.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="wrapper">
+    <header class="shadow p-3 mb-5 bg-body-tertiary rounded">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CMS Aldo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Page
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Page</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+                <li class="nav-item">
+          <a class="nav-link" href="user.php">User</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </header>
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="card">
+              <div class="card-header">
+                Data User
+              </div>
+              <div class="card-body">
+                <form action="" method="post">
+                  <div class="mb-3 row">
+                    <div class="col-sm-2">
+                      <label for="">Nama</label>
+                    </div>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" placeholder="Masukkan nama anda">
+                    </div>
+                  </div>
+                  <div class="mb-3 row">
+                    <div class="col-sm-2">
+                      <label for="">Email</label>
+                    </div>
+                    <div class="col-sm-10">
+                      <input type="Email" class="form-control" placeholder="Masukkan email anda">
+                    </div>
+                  </div>
+                  <div class="mb-3 row">
+                    <div class="col-sm-2">
+                      <label for="">Password</label>
+                    </div>
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" placeholder="Masukkan password anda">
+                    </div>
+                  </div>
+                  <div class="mb-3 row">
+                    <div class="col-sm-12">
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                  </div>
+                </form>
+
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ admin/user.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="wrapper">
+    <header class="shadow p-3 mb-5 bg-body-tertiary rounded">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CMS Aldo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Page
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Page</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+                <li class="nav-item">
+          <a class="nav-link" href="user.php">User</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </header>
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="card">
+              <div class="card-header">
+                Data User
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <div align="right" class="mb-3">
+                    <a href="tambah-user.php" class="btn btn-primary">Add</a>
+                  </div>
+                  <table class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Aldo</td>
+                        <td>aldorio12@gmail.com</td>
+                        <td>
+                          <a href="" class="btn btn-success btn-sm">Edit</a>
+                          <a onclick="return confirm('Are u Sure?')" href="" class="btn btn-warning btn-sm">Delete</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
